eac27d5099bc666f25b8b8492285bf960d74583d
Barton Satchwill <barton.satchwill@cybera.ca>
2015-04-15 13:58:37 -0600

    db creation fixes, demo tables match generated sensor data

--- rdw/templates/default/createdb.sql
+++ rdw/templates/default/createdb.sql
@@ -5,28 +5,30 @@
 CREATE ROLE :user WITH LOGIN PASSWORD ':passwd';
 CREATE DATABASE :db OWNER :user;
 GRANT ALL PRIVILEGES ON DATABASE :db TO :user;
+\connect :db
 
-CREATE TABLE films (
-    code        char(5) CONSTRAINT firstkey PRIMARY KEY,
-    title       varchar(40) NOT NULL,
-    did         integer NOT NULL,
-    date_prod   date,
-    kind        varchar(10),
-    len         interval hour to minute
+CREATE TABLE sensor_reading (
+    id          char(5) CONSTRAINT firstkey PRIMARY KEY,
+    flux        numeric(6, 4),
+    sagans      numeric(4, 2),
+    fizz        numeric(6, 4),
+    sparkle     numeric(4, 2),
+    clarity     numeric(3, 1),
+    sputz       numeric(7, 5),
+    read_date   date
 );
 
-CREATE TABLE distributors (
+CREATE TABLE fibble (
      did    integer PRIMARY KEY,
      name   varchar(40) NOT NULL CHECK (name <> '')
 );
 
-ALTER TABLE distributors ADD COLUMN address varchar(30);
-ALTER TABLE distributors ADD COLUMN name varchar(30);
-ALTER TABLE distributors ADD COLUMN name2 varchar(30);
+ALTER TABLE sensor_reading ADD COLUMN hab varchar(30);
+ALTER TABLE sensor_reading ADD COLUMN zapple varchar(30);
+ALTER TABLE sensor_reading ADD COLUMN sputz_too numeric(7, 5);
 
-ALTER TABLE distributors
-    ALTER COLUMN address TYPE varchar(80),
+update sensor_reading set sputz_too = 2*sputz;
+
+ALTER TABLE fibble
+    ADD COLUMN address varchar(80),
     ALTER COLUMN name TYPE varchar(100);
-
-update distributors set name2 = name;
-
