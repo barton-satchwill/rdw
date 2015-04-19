80deac212312746a144c5f4621896da6ec407166
barton <barton.satchwill@gmail.com>
2015-04-19 10:43:58 -0600

    move solo files into directory

---
+++ solo/backup.rb
@@ -0,0 +1,7 @@
+root = File.absolute_path(File.dirname(__FILE__))
+root = File.join(root, "..")
+
+file_cache_path "/tmp" 
+cookbook_path [File.join(root,"site-cookbooks"), root]
+json_attribs File.join(root, "solo/backup.json") 
+
